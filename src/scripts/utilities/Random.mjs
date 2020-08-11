0810c1fe7ad98f9eddb2f007a2a9ea5cd958233a
Tim <jasper7466@yandex.ru>
2020-08-11 19:49:28 +0500

    Random. Добавлена утилита для получения случайного целого числа в интервале

---
+++ src/scripts/utilities/Random.mjs
@@ -0,0 +1,7 @@
+// Утилита для получения случайного целого числа в заданном интервале, включительно
+export function RandomIntInclusive(min, max)
+{
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
+}
