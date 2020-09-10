7e32d7ccbcf14d8103493c9c93b6669161e19b51
Tim <jasper7466@yandex.ru>
2020-09-10 23:08:00 +0500

    Config. Создан файл конфигурации

---
+++ src/scripts/config.js
@@ -0,0 +1,20 @@
+// Конфигурационный файл
+
+// Набор изображений
+export const images = {
+    tile: require('../images/tile.png'),                    // Обесцвеченный спрайт тайла
+    field: require('../images/field.png'),                  // Фон игрового поля
+    moves: require('../images/moves.png'),                  // Фон метки "Ходы"
+    bar: require('../images/bar.png'),                      // Спрайт полосы прогресса
+    barBack: require('../images/bar_back.png'),             // Спрайт фона полосы прогресса
+    scorePanel: require('../images/score_panel.png'),       // Фон панели очков/ходов
+    topPanel: require('../images/top_panel.png'),           // Фон верхней панели
+    background: require('../images/background.png'),        // Фон игровой сцены
+    progressPanel: require('../images/progress_panel.png'), // Фон панели прогресса
+    pauseBase: require('../images/pause_base.png'),         // Кнопка "Пауза". Базовый спрайт
+    pauseHover: require('../images/pause_hover.png'),       // Кнопка "Пауза". Спрайт наведения
+    pausePress: require('../images/pause_press.png'),       // Кнопка "Пауза". Спрайт нажатия
+    buttonBase2: require('../images/button2_base.png'),     // Кнопка, тип 2. Базовый спрайт
+    buttonHover2: require('../images/button2_hover.png'),   // Кнопка, тип 2. Спрайт наведения
+    buttonPress2: require('../images/button2_press.png')    // Кнопка, тип 2. Спрайт нажатия
+}
